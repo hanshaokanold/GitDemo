997ad80753c3863355c6cb3ea05aca33a456079a
hanshaokan <hsk294245303@126.com>
2021-03-14 11:39:25 +0800

    第三次修改代码，增加了体重属性

--- Module01/src/GItTest.java
+++ Module01/src/GItTest.java
@@ -2,6 +2,7 @@ public class GItTest {
 
     private String name;
     private int age;
+    private double weight;
     public static void main(String[] args) {
         System.out.println("hello Git");
     }
